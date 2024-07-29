3dce468c325e9186fed6af5278de61c72c455a41
Darya Kolenchenko <daria.kolenchenko@gmail.com>
2024-07-29 14:05:29 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
+  function match10(n) {
+    switch(n) {
+        case 0: return 'zero';
+        case 1: return 'one';
+        case 2: return 'two';
+        case 3: return 'three';
+        case 4: return 'four';
+        case 5: return 'five';
+        case 6: return 'six';
+        case 7: return 'seven';
+        case 8: return 'eight';
+        case 9: return 'nine';
+    }
+  }
+  function match20(n) {
+    switch(n) {
+        case 10: return 'ten';
+        case 11: return 'eleven';
+        case 12: return 'twelve';
+        case 13: return 'thirteen';
+        case 14: return 'fourteen';
+        case 15: return 'fifteen';
+        case 16: return 'sixteen';
+        case 17: return 'seventeen';
+        case 18: return 'eightteen';
+        case 19: return 'nineteen';
+    }
+  }
+
   
 }
